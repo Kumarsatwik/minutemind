fa4d5f3e779735968f69704fd149542987583ad7
kumarsatwik <kumarsatwik6@gmail.com>
2025-10-30 01:52:29 +0530

    feat: add S3 bucket remote pattern for image optimization

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,13 @@ const nextConfig: NextConfig = {
   /* config options here */
   images: {
     domains: ["img.clerk.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "minutemind-bot.s3.ap-south-1.amazonaws.com",
+        pathname: "/**",
+      },
+    ],
   },
 };
 
